3980a3129d515293a590ce3a9c3e4bf0d905745c
lidaoshanying <lidaoshanying@163.com>
2017-07-21 11:15:53 +0800

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,8 +41,6 @@ Route::get('expert/detail/{expertId}','ExpertController@detail');
 Route::get('supply','SupplyController@index');
 //供求信息详情
 Route::get('supply/detail/{supplyId}','SupplyController@detail');
-<<<<<<< HEAD
-=======
 
 /**************************************收藏留言相关路由*********************************************/
 //供求收藏
@@ -54,7 +52,6 @@ Route::post('dealextcollect','ExpertController@dealCollect');
 //专家留言
 Route::post('replyextmessage','ExpertController@replyMessage');
 
->>>>>>> origin/lds
 /**************************************个人中心的路由***********************************************/
 //基本资料
 Route::get('uct_basic','CenterController@index');
