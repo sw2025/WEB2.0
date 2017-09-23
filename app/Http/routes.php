eebe2a9cd41fbd6624eb32adb27b3159eb8648b3
1874207723 <1874207723@qq.com>
2017-09-23 18:11:22 +0800

    wn3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -98,6 +98,8 @@ Route::post('replyextmessage','ExpertController@replyMessage');
     Route::post('applicationCashs', 'CenterController@applicationCashs');
 //提现验证银行卡
 Route::post('haveCard', 'CenterController@haveCard');
+//专家绑定银行卡
+Route::post('expertHaveCard', 'ExpertUcenterController@expertHaveCard');
 //获取充值记录
     Route::post('getRecord', 'CenterController@getRecord');
 //消息标记已读
