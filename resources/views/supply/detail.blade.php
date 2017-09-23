299314b95524c19f816dfe6d2411fb21779469ff
jiang <192793660@qq.com>
2017-09-23 16:55:31 +0800

    9/23

--- resources/views/supply/detail.blade.php
+++ resources/views/supply/detail.blade.php
@@ -60,7 +60,7 @@
                             @if(!$v->parentid)
                             <div class="mes-list-box clearfix">
                                 <div class="floor-host">
-                                    <img src="{{env('ImagePath').$v->avatar}}" class="floor-host-ava" />
+                                    <img src="@if(empty($v->avatar)){{url('img/avatar.jpg')}}@else {{env('ImagePath').$v->avatar}}@endif" class="floor-host-ava" />
                                     <div class="floor-host-desc">
                                         <a href="javascript:;" class="floor-host-name">{{$v->nickname}} [{{$v->enterprisename or $v->expertname}}]</a><span class="floor-host-time">{{$v->messagetime}}</span>
                                         <span class="floor-host-words">{{$v->content}}</span>
@@ -75,7 +75,7 @@
                                         @foreach($message as $reply)
                                             @if(!$reply->use_userid && $reply->parentid == $v->id)
                                             <li>
-                                                <img src="{{env('ImagePath').$reply->avatar}}" class="floor-guest-ava" />
+                                                <img src="@if(empty($reply->avatar)){{url('img/avatar.jpg')}}@else {{env('ImagePath').$reply->avatar}}@endif" class="floor-guest-ava" />
                                                 <div class="gloor-guest-cnt">
                                                     <a href="javascript:;" class="floor-guest-name">{{$reply->nickname or substr_replace($reply->phone,'****',3,4)}}</a>
                                                     <span class="floor-guest-words">{{$reply->content}}</span>
@@ -87,7 +87,7 @@
                                             @elseif($reply->parentid == $v->id)
 
                                             <li>
-                                                <img src="{{env('ImagePath').$reply->avatar}}" class="floor-guest-ava" />
+                                                <img src="@if(empty($reply->avatar)){{url('img/avatar.jpg')}}@else {{env('ImagePath').$reply->avatar}}@endif" class="floor-guest-ava" />
                                                 <div class="gloor-guest-cnt">
                                                     <a href="javascript:;" class="floor-guest-name">{{$reply->nickname or substr_replace($reply->phone,'****',3,4)}}</a>回复&nbsp;<a href="javascript:;" class="floor-guest-name">{{$reply->nickname2 or substr_replace($reply->phone2,'****',3,4)}}</a>
                                                     <span class="floor-guest-words">{{$reply->content}}</span>
