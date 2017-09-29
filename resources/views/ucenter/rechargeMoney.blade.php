15008c34898202d3ac84ad3f23c1fe9ed008fab4
shan <lidaoshan@163.com>
2017-09-29 14:55:37 +0800

    9.29 山
    9.29 山

--- resources/views/ucenter/rechargeMoney.blade.php
+++ resources/views/ucenter/rechargeMoney.blade.php
@@ -5,6 +5,21 @@
             <!-- 充值 / start -->
             <div class="ucenter-con">
                 <div class="main-right">
+                    <div class="view">
+                        <strong class="view-fz">余额用途说明:</strong>
+                        <div>1.用于办事
+                            <span>企业可以通过平台的办事管理解决企业遇到的难题，本平台会从余额中扣除每次的费用。</span>
+                        </div>
+                        <div>2.视频咨询
+                            <span>企业可以通过平台的视频咨询模块直接对话办事专家，平台会从余额中向专家支付费用,该费用由专家设置每5分钟所收的价格,另外视频时，平台还会从余额中扣取每五分钟2元的流量费。</span>
+                        </div>
+                        <div>3.线下优惠
+                            <span>本平台线下会有一些课程，需要的用户也会从余额中扣除费用。</span>
+                        </div>
+                        <div>4.提现
+                            <span>本平台余额在达到1000元以上还可以进行提现操作。</span>
+                        </div>
+                    </div>
                     <div class="recharge-sum">
                             <span class="recharge-opt focus">
                                 <input class="rad-inp" checked="true" type="radio" id="rad1" name="money">
@@ -23,6 +38,7 @@
                                 <label for="rad4" class="recharge-radio"><span></span>其他金额</label><input type="text" placeholder="请输入金额" readonly="" class="recharge-inp-sum">
                             </span>
                     </div>
+
                     <div class="recharge-way">
                             <span class="recharge-opt focus">
                                 <input class="rad-inp" checked="true" type="radio" id="way1" name="ways">
